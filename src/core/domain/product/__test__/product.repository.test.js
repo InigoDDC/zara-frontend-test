0f41b662050483e8a24704016d3a1ff9ec3dc600
Iñigo <inigo.diazdecerio@biko2.com>
2020-12-08 23:12:15 +0100

    Add cache functionality to the repositoty

--- src/core/domain/product/__test__/product.repository.test.js
+++ src/core/domain/product/__test__/product.repository.test.js
@@ -1,39 +1,78 @@
 import * as productMappers from 'core/domain/product/product.mappers'
 import { apiClient } from 'core/infrastructure/apiClient'
 import { apiRouteProvider } from 'core/infrastructure/apiRouteProvider'
+import { localStorageService } from 'core/infrastructure/localStorageService'
 import { productRepository } from '../product.repository'
 import { aProduct, aProductDetail, aProductDto, aProductDetailDto } from './product.testDataFactory'
 
 describe('Product repository', () => {
-  it('loads the product list and maps each product', async () => {
-    const expectedProductDtoList = [aProductDto({}), aProductDto({})]
-    const expectedProduct = aProduct({})
-    const expectedProductList = [expectedProduct, expectedProduct]
-    const apiMock = jest.spyOn(apiClient, 'get').mockResolvedValue(expectedProductDtoList)
-    const mapperMock = jest.spyOn(productMappers, 'mapProductDtoToProduct').mockReturnValue(expectedProduct)
+  describe('when loading the productList', () => {
+    it('loads the product list, maps each product and saves it if there is no cache', async () => {
+      const expectedProductDtoList = [aProductDto({}), aProductDto({})]
+      const expectedProduct = aProduct({})
+      const expectedProductList = [expectedProduct, expectedProduct]
 
-    const productList = await productRepository.loadProductList()
+      const localStorageGetMock = jest.spyOn(localStorageService, 'get').mockReturnValue(undefined)
+      const apiMock = jest.spyOn(apiClient, 'get').mockResolvedValue(expectedProductDtoList)
+      const mapperMock = jest.spyOn(productMappers, 'mapProductDtoToProduct').mockReturnValue(expectedProduct)
+      const localStorageSaveMock = jest.spyOn(localStorageService, 'save').mockImplementation(() => '')
 
-    expect(apiMock).toBeCalledWith(apiRouteProvider.productList)
-    expect(mapperMock).toBeCalledTimes(expectedProductDtoList.length)
-    expect(productList).toEqual(expectedProductList)
+      const productList = await productRepository.loadProductList()
+
+      expect(localStorageGetMock).toBeCalled()
+      expect(apiMock).toBeCalledWith(apiRouteProvider.productList)
+      expect(mapperMock).toBeCalledTimes(expectedProductDtoList.length)
+      expect(localStorageSaveMock).toBeCalledWith(expect.anything(), expectedProductList)
+      expect(productList).toEqual(expectedProductList)
+    })
+
+    it('loads the product list from the cache if it exists', async () => {
+      const expectedProduct = aProduct({})
+      const expectedProductList = [expectedProduct, expectedProduct]
+
+      const localStorageGetMock = jest.spyOn(localStorageService, 'get').mockReturnValue(expectedProductList)
+
+      const productList = await productRepository.loadProductList()
+
+      expect(localStorageGetMock).toBeCalled()
+      expect(productList).toEqual(expectedProductList)
+    })
   })
 
-  it('loads the product detail and maps it', async () => {
-    const apiPath = 'path'
-    const expectedProductDetailDto = aProductDetailDto({})
-    const expectedProductDetail = aProductDetail({})
+  describe('when loading a product detail', () => {
+    it('loads the product detail, maps it and saves it if there is no cache', async () => {
+      const apiPath = 'path'
+      const productId = 'id'
+      const expectedProductDetailDto = aProductDetailDto({})
+      const expectedProductDetail = aProductDetail({})
 
-    jest.spyOn(apiRouteProvider, 'productDetail').mockReturnValue(apiPath)
-    const apiMock = jest.spyOn(apiClient, 'get').mockResolvedValue(expectedProductDetailDto)
-    const mapperMock = jest
-      .spyOn(productMappers, 'mapProductDetailDtoToProductDetail')
-      .mockReturnValue(expectedProductDetail)
+      jest.spyOn(apiRouteProvider, 'productDetail').mockReturnValue(apiPath)
+      const localStorageGetMock = jest.spyOn(localStorageService, 'get').mockReturnValue(undefined)
+      const apiMock = jest.spyOn(apiClient, 'get').mockResolvedValue(expectedProductDetailDto)
+      const mapperMock = jest
+        .spyOn(productMappers, 'mapProductDetailDtoToProductDetail')
+        .mockReturnValue(expectedProductDetail)
+      const localStorageSaveMock = jest.spyOn(localStorageService, 'save').mockImplementation(() => '')
 
-    const productDetail = await productRepository.loadProductDetail('id')
+      const productDetail = await productRepository.loadProductDetail(productId)
 
-    expect(apiMock).toBeCalledWith(apiPath)
-    expect(mapperMock).toBeCalledWith(expectedProductDetailDto)
-    expect(productDetail).toEqual(expectedProductDetail)
+      expect(localStorageGetMock).toBeCalledWith(`productDetail-${productId}`)
+      expect(apiMock).toBeCalledWith(apiPath)
+      expect(mapperMock).toBeCalledWith(expectedProductDetailDto)
+      expect(localStorageSaveMock).toBeCalledWith(`productDetail-${productId}`, expectedProductDetail)
+      expect(productDetail).toEqual(expectedProductDetail)
+    })
+
+    it('loads the product detail from the cache if it exists', async () => {
+      const productId = 'id'
+      const expectedProductDetail = aProductDetail({})
+
+      const localStorageGetMock = jest.spyOn(localStorageService, 'get').mockReturnValue(expectedProductDetail)
+
+      const productDetail = await productRepository.loadProductDetail(productId)
+
+      expect(localStorageGetMock).toBeCalledWith(`productDetail-${productId}`)
+      expect(productDetail).toEqual(expectedProductDetail)
+    })
   })
 })
--- src/core/domain/product/product.repository.js
+++ src/core/domain/product/product.repository.js
@@ -1,15 +1,30 @@
 import { apiClient } from 'core/infrastructure/apiClient'
 import { apiRouteProvider } from 'core/infrastructure/apiRouteProvider'
+import { localStorageService } from 'core/infrastructure/localStorageService'
 import { mapProductDetailDtoToProductDetail, mapProductDtoToProduct } from './product.mappers'
 
 const loadProductList = async () => {
+  const productListCache = localStorageService.get('productList')
+  if (productListCache) {
+    return productListCache
+  }
+
   const productDtoList = await apiClient.get(apiRouteProvider.productList)
-  return productDtoList.map(mapProductDtoToProduct)
+  const mappedDtoList = productDtoList.map(mapProductDtoToProduct)
+  localStorageService.save('productList', mappedDtoList)
+  return mappedDtoList
 }
 
 const loadProductDetail = async productId => {
+  const productListCache = localStorageService.get(`productDetail-${productId}`)
+  if (productListCache) {
+    return productListCache
+  }
+
   const productDetailDto = await apiClient.get(apiRouteProvider.productDetail(productId))
-  return mapProductDetailDtoToProductDetail(productDetailDto)
+  const mappedProductDetail = mapProductDetailDtoToProductDetail(productDetailDto)
+  localStorageService.save(`productDetail-${productId}`, mappedProductDetail)
+  return mappedProductDetail
 }
 
 export const productRepository = {
